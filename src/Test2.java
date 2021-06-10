87c0d8511cb08d9cf9651df14f6b5d491b2c6980
郭鑫 <guoxin@ihaoshenghuo.com>
2021-06-10 10:14:16 +0800

    5555

--- src/Test2.java
+++ src/Test2.java
@@ -7,6 +7,7 @@
 public class Test2 {
 
     public static void main(String[] args) {
-        System.out.println(456456);
+        System.out.println(45645645);
+        System.out.println(45645645);
     }
 }
