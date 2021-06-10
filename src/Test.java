7f1bf7d52f06aa94d36a52c60a27ab89cfdf7799
郭鑫 <guoxin@ihaoshenghuo.com>
2021-06-10 09:18:37 +0800

    11

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,12 @@
-package PACKAGE_NAME;/**
+/**
  * TODO
  *
  * @author guoxin
  * @date 2021/6/10 9:15
- */public class Test {
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
 }
